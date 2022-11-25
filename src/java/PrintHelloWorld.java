7d68c87236dfe5a44d70104f05c683db41bc69eb
mhmhmh <gMHMHMHg@gmail.com>
2022-11-25 22:12:27 +0900

    first

--- src/java/PrintHelloWorld.java
+++ src/java/PrintHelloWorld.java
@@ -2,6 +2,6 @@ package java;
 
 public class PrintHelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world ! hi");
+        System.out.println("hello world ! hi dd");
     }
 }
